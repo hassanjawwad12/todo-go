e79b6460fec5fb8693a367caca205e61ee1e0b85
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2025-01-01 17:32:47 +0500

    Created the Todo Struct and Array of Todos

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # todo-go
 To-do list Rest API in go
+
+# Install 
+`go mod init github.com/hassanjawwad12/todo-go`
--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Todo struct {
+	Id   string `json: "id", required`
+	Note string `json: "note", required`
+}
+
+// Created array of 3 todos
+var todos = []Todo{
+	{Id: "1", Note: "Complete the project"},
+	{Id: "2", Note: "Make the pull request"},
+	{Id: "3", Note: "Read a book by Dostoevsky"},
+}
+
+func GetTodos(context *gin.Context) {
+
+}
+
 func main() {
+
+	router := gin.Default()
+	router.GET("/todos")
+	router.Run("localhost:8080") // Run our server
+
+	// The server only accepts JSON
 
 }
